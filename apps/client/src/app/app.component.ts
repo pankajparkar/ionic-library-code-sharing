5b00f8c14d963eaeff293f843929eeaa42526368
Pankaj Parkar <parkarpankaj3@gmail.com>
2021-02-08 21:29:06 +0800

    Added nx workspace with ionic plugin.

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ionic-library-code-sharing-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'client';
+}
