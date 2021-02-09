8c959cefa406ced0dec8b8624a4e83b589e28476
Pankaj Parkar <parkarpankaj3@gmail.com>
2021-02-09 19:37:44 +0800

    Added `navbar` component in CoreModule.

--- libs/core/src/lib/core.module.ts
+++ libs/core/src/lib/core.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, IonicModule],
+  declarations: [NavbarComponent],
+  exports: [NavbarComponent],
 })
 export class CoreModule {}
